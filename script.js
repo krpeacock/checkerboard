9c897d7e220fa14cce571afc0c80a33225aa0431
krpeacock <kylpeacock@gmail.com>
2016-02-29 16:48:01 -0800

    the number of tiles is sloppy

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 var body = document.getElementsByTagName(body);
 
-function randomColor() {
-    var x = Math.round(0xffffff * Math.random()).toString(16);
-    var y = (6 - x.length);
-    var z = "000000";
-    var z1 = z.substring(0, y);
-    var color = "#" + z1 + x;
+function gradient() {
+    
 return color;}
 
 function board() {
 
     for (i = 0; i < window.innerHeight / 5; i++) {
-        document.body.innerHTML += `<div style="display:inline-block;width:11.1%;background-color:${randomColor()};margin:0;padding-bottom:11.1%;float:left;"></div>`;
+        if (i%2){
+        document.body.innerHTML += `<div style="display:inline-block;width:11.1%;background-color:blue;margin:0;padding-bottom:11.1%;float:left;"></div>`;
+    } else {
+        document.body.innerHTML += `<div style="display:inline-block;width:11.1%;background-color:purple;margin:0;padding-bottom:11.1%;float:left;"></div>`;
     }
-}
+}}
 board();
+
+function gradient(){
+    document.body.innerHTML += `<div style="position:fixed;height:100vh;width:100%;background:linear-gradient(#9900cc, #ff5050);opacity:.6;z-index:2;left:-10px;"></div>`
+}
+gradient();
