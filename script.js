8aa84ff6f40c0519907da390d7e4d5ced1389043
krpeacock <kylpeacock@gmail.com>
2016-02-29 16:20:11 -0800

    random colors ftw

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 var body = document.getElementsByTagName(body);
 
+function randomColor() {
+    var x = Math.round(0xffffff * Math.random()).toString(16);
+    var y = (6 - x.length);
+    var z = "000000";
+    var z1 = z.substring(0, y);
+    var color = "#" + z1 + x;
+return color;}
+
 function board() {
-    for (i = 0; i < window.innerHeight/5; i++) {
-        if (i % 2) {
-            document.body.innerHTML += '<div style="display:inline-block;width:11.1%;background-color:red;margin:0;padding-bottom:11.1%;float:left;"></div>';
-        } else {
-            document.body.innerHTML += '<div style="display:inline-block;width:11.1%;background-color:black;margin:0;padding-bottom:11.1%;float:left;"></div>';
-        }
-    }}
-    board();
+
+    for (i = 0; i < window.innerHeight / 5; i++) {
+        document.body.innerHTML += `<div style="display:inline-block;width:11.1%;background-color:${randomColor()};margin:0;padding-bottom:11.1%;float:left;"></div>`;
+    }
+}
+board();
